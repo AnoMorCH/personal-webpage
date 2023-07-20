dc435d63d12855d4163d8f246aac435c24a10f61
AnoMorCH <anomorch@gmail.com>
2023-07-20 09:11:32 +0300

    translation of rating of cities

---
+++ static/scripts/language-switches/projects/rating-of-cities.js
@@ -0,0 +1,7 @@
+// Switch a language of projects/rating-of-cities.html template.
+
+const currentLanguage = getCurrentLanguage();
+// TODO. Think if there is a better name for the var below as "template" can
+// TODO. have a lot of different interpretations.
+const templatePath = "/static/language/projects/rating-of-cities.json";
+switchLanguage(currentLanguage, templatePath);
