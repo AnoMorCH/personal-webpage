05390f5355bd7fec99db4a12f8089cd265329bd4
AnoMorCH <anomorch@gmail.com>
2023-07-20 09:31:52 +0300

    translation of graphs-of-rating-of-cities

---
+++ static/scripts/language-switches/projects/graphs-of-rating-of-cities.js
@@ -0,0 +1,5 @@
+// Switch a language of projects/graphs-of-rating-of-cities.html template.
+
+const currentLanguage = getCurrentLanguage();
+const templatePath = "/static/language/projects/graphs-of-rating-of-cities.json";
+switchLanguage(currentLanguage, templatePath);
