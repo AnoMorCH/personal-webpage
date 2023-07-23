73149a317c6c4b0f431ebea11d6a62384a0c4f3a
AnoMorCH <anomorch@gmail.com>
2023-07-23 09:24:25 +0300

    close useless todo

--- static/scripts/language-switches/templates-translation/projects/rating-of-cities.js
+++ static/scripts/language-switches/templates-translation/projects/rating-of-cities.js
@@ -4,8 +4,6 @@ switchRatingOfCitiesLanguage();
 
 function switchRatingOfCitiesLanguage() {
   const currentLanguage = getCurrentLanguage();
-  // TODO. Think if there is a better name for the var below as "template" can
-  // TODO. have a lot of different interpretations.
   const templatePath = "/static/language/projects/rating-of-cities.json";
   switchLanguage(currentLanguage, templatePath);
 }
