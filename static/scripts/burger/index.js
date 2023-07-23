a555648daa1ec1576899f3a69e8c11a029215e29
AnoMorCH <anomorch@gmail.com>
2023-07-23 09:24:08 +0300

    improve navmenu (hide fields, rename "object")

--- static/scripts/burger/index.js
+++ static/scripts/burger/index.js
@@ -1,37 +1,41 @@
 class NavMenu {
-  // TODO. Hide all fields.
+  #openBtn;
+  #closeBtn;
+  #navMenu;
+  #main;
+  #headerGrid;
+
   constructor() {
-    this.openBtn = document.getElementById("burger");
-    this.closeBtn = document.getElementById("close-burger-menu-btn");
-    // TODO. Rename "object".
-    this.object = document.getElementById("burger-items");
-    this.main = document.getElementsByTagName("main")[0];
-    this.headerGrid = document.getElementById("grid");
+    this.#openBtn = document.getElementById("burger");
+    this.#closeBtn = document.getElementById("close-burger-menu-btn");
+    this.#navMenu = document.getElementById("burger-items");
+    this.#main = document.getElementsByTagName("main")[0];
+    this.#headerGrid = document.getElementById("grid");
   }
 
   #open() {
-    this.object.style.transform = "translateY(100vh)";
+    this.#navMenu.style.transform = "translateY(100vh)";
     this.#hideMainContent();
   }
   
   #close() {
-    this.object.style.transform = "translateY(0)";
+    this.#navMenu.style.transform = "translateY(0)";
     this.#showMainContent();
   }
 
   #hideMainContent() {
-    this.main.style.display = "none";
-    this.headerGrid.display = "none";
+    this.#main.style.display = "none";
+    this.#headerGrid.display = "none";
   }
 
   #showMainContent() {
-    this.main.style.display = "block";
-    this.headerGrid.display = "grid";
+    this.#main.style.display = "block";
+    this.#headerGrid.display = "grid";
   }
 
   manageEventsToOpenClose() {
-    this.openBtn.addEventListener("click", () => { this.#open(); });
-    this.closeBtn.addEventListener("click", () => { this.#close(); });
+    this.#openBtn.addEventListener("click", () => { this.#open(); });
+    this.#closeBtn.addEventListener("click", () => { this.#close(); });
   }
 }
 
